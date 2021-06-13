220552516fb0e59280032c6e730dc5b2ec024b34
Furey <bordonnet@hotmail.com>
2021-06-13 16:24:37 -0300

    V2 y Educacon

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,25 @@
-let links = document.getElementsByClassName('btn-floating');
 
+window.onload = resizer;
+window.onresize = resizer;
 
-for (var i = 0; i < links.length; i++) {
-    const element = links[i];
-    element.addEventListener("mouseover", function (event) {
-        element.classList += 'shadowplus ';
-    }, false);
-    
-    element.addEventListener("mouseout", function (event) {
-        element.classList[-1].replace('')
-    }, false);
+function resizer(params) {
+    personal_card();
+    slant_image();
 }
 
+function slant_image() {
+    const profile_img = document.getElementById('profile-image');
+    const profile_slant = document.getElementById('profile-slant');
+
+    let height = profile_img.height;
+    let width = profile_img.width;
+
+    profile_slant.style = "border-right: " + width / 4 + 'px solid transparent; border-bottom: ' + height + 'px solid #0194fe;';
+};
+
+function personal_card() {
+    const pci = document.getElementById('personal-card-information');
+    const pcimg = document.getElementById('personal-card-image');
+
+    pcimg.style = 'height: ' + pci.clientHeight + 'px;';
+};
